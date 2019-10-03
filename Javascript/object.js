28102f1fb59537cc8900dd1dac3b53c23450bef3
tyogautomo <yoga.utomo27@gmail.com>
2019-10-03 10:22:02 +0700

    prime done

---
+++ Javascript/object.js
@@ -0,0 +1,6 @@
+let data = [{ "username": "ali", "hair_color": "brown", "height": 1.2 }, {
+    "username": "mar
+c","hair_color":"blue","height":1.4},{"username":"joe","hair_color":"b
+rown","height":1.7},{"username":"zehua","hair_color":"black","height":
+1.8
+}]
---
+++ NodeJS/prime.js
@@ -0,0 +1,24 @@
+const getPrimesLowerThan = (number) => {
+    if (number > 0) {
+        let primes = []
+        // because 1 is not a prime number, so start it with 2
+        for (let i = 2; i < number; i++) {
+            let check = true
+            for (let j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    check = false
+                }
+            }
+            if (check) primes.push(i)
+        }
+        return primes
+    } else {
+        return 'Please input number more than 0 (zero)'
+    }
+}
+
+console.log(getPrimesLowerThan(-1))
+console.log(getPrimesLowerThan(5))
+console.log(getPrimesLowerThan(10))
+console.log(getPrimesLowerThan(20))
+console.log(getPrimesLowerThan(100))
